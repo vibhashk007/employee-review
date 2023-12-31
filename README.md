1b90b321fabcece77aadad2fc0b6b67dd6554a94
cherdid <kumarvibhash19@gmail.com>
2023-12-31 09:53:11 +0530

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# employee-review
+# EMPLOYEE REVIEW SYSTEM
+
+An employee review system that allows employees to submit feedback towards each other's performance made using nodejs and ejs along with mongoDB.
+
+## DEMO
+
+## Installation
+
+To install The project locally follow these steps:
+
+- Clone the repositry on your local machine
+  > git clone https://github.com/vibhashk007/employee-review.git
+- Install all the dependencies:
+
+  - npm install
+  - npm i nodemon
+
+- Start the local server
+  > npm start
+
+## Contact
+
+Author - [Vibhash](https://github.com/vibhashk007/)
---
+++ index.js
@@ -0,0 +1,70 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
+const db = require("./config/mongoose");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocal = require("./config/passport-local-strategy");
+const connectMongo = require("connect-mongo");
+const MongoStore = connectMongo(session);
+const flash = require("connect-flash");
+const customMware = require("./config/middleware.js");
+const path = require("path");
+const { PORT, SECRET_KEY } = process.env;
+
+// Add the following lines to configure body-parser
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.use(express.static("./assets"));
+app.use(expressLayouts);
+
+// extract style and script from sub pages into the layout
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.use(
+  session({
+    secret: "secretkey",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 1000 * 60 * 20,
+    },
+    store: new MongoStore(
+      {
+        mongooseConnection: db,
+        autoRemove: "disable",
+      },
+      function (err) {
+        console.log(err || "connect-mongodb setup ok");
+      }
+    ),
+  })
+);
+
+app.use(passport.initialize());
+
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
+
+app.use(flash());
+
+app.use(customMware.setFlash);
+
+app.use("/", require("./routes"));
+
+app.listen(PORT || 5000, (err) => {
+  if (err) {
+    console.log(`Error is running the server: ${err}`);
+  }
+  console.log(`server is running on port: ${PORT}`);
+});
