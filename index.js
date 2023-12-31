f07ea86d481def868d3adf91ae86fa51ad0523e2
cherdid <kumarvibhash19@gmail.com>
2023-12-31 10:31:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
-app.use(express.static("assets"));
+app.use(express.static("./assets"));
 app.use(expressLayouts);
 
 // extract style and script from sub pages into the layout
